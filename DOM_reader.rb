99bd2a6467b1b18d1f8946fb7c35897bd491cabd
Justin Mullis <justin.mullis@gmail.com>
2015-08-22 21:49:38 -0400

    dom_tree parser complete

--- DOM_reader.rb
+++ DOM_reader.rb
@@ -3,3 +3,69 @@
 # node.  We then search through the "data" of that element to determine if it has
 # any children, and so on until the entire document has been parsed into a tree.
 
+require_relative 'tag_parser.rb'
+
+Node = Struct.new(:name, :text, :classes, :id, :children, :parent, :raw_data, 
+                  :raw_child_tags)
+
+class DOMReader
+  include TagParser
+
+  attr_reader :root
+
+  def initialize(html_file)
+    # The root is the html element and its raw_data is the entire document 
+    # except doctype and the html opening and closing tags
+    raw_html = File.read(html_file)
+    @root = Node.new
+    @root.name = :html
+    trimmed_html = get_instance_of_tag("html", raw_html)
+    @root.raw_data = trimmed_html[1]
+  end
+
+
+  def build_tree
+    queue = [@root]
+
+    until queue.empty?
+      node = queue.shift
+      node.children = []
+
+      # nodes will start with raw_data, we need to seperate this out 
+      # into raw_child_tags and any non-HTML text the node contains
+      # we get to a base case when a node has no children, in this case
+      # its raw data will be all non-HTML text and the each loop below 
+      # will just be skipped over (raw_child_tags will be empty)
+      seperate_text_from_tags(node)
+
+      node.raw_child_tags.each do |raw_tag|
+        # we get the first tag of the raw_child, which is the actual
+        # child node we're constructing
+        tag = get_first_tag(raw_tag)
+        # get the tag attributes
+        child_attr = parse_tag(tag)
+        # create a new node and setup attributes
+        child = Node.new
+        child.name = child_attr[:name]
+        child.id = child_attr[:id]
+        child.classes = child_attr[:classes]
+
+        child.parent = node
+
+        # now we setup the child's raw data
+        raw_data = get_instance_of_tag(child.name.to_s, raw_tag)
+        child.raw_data = raw_data[1]
+
+        node.children << child
+        queue << child
+      end
+
+      #raw_child_tags is no longer needed once all the children have been built
+      node.raw_child_tags.clear
+
+    end
+   
+  end
+  
+
+end
--- tag_parser.rb
+++ tag_parser.rb
@@ -12,7 +12,7 @@ module TagParser
         values = get_attribute_values(attr)
         tag_data[name.to_sym] = values
       end 
-      
+
     end
     
     tag_data[:classes] = tag_data.delete(:class) if tag_data.has_key?(:class)
@@ -45,6 +45,55 @@ module TagParser
     else
       return values
     end
+  end
+
+
+  def get_first_tag(text)
+    # regex <\s*\w.*?> to return all start tags in text
+    # get_tag_name(result[0])
+    all_tags = text.scan(/<\s*\w.*?>/)
+    all_tags[0]  
+  end
+
+
+  # returns the first instance of a tag, the match result is the tag and
+  # everything it contains, the match group (match[1]) is just the contents of
+  # the tag
+  def get_instance_of_tag(tag, search_text)
+    # issues come up with greedy vs non-greedy regex when running this search
+    # and you need to determine if you're dealing with a tag that has siblings
+    # (<li></li><li></li>) or nested children (<div><div></div></div>) of the same
+    # element name, for now, will fail with nested children
+    consecutive_regex = /<\s*#{tag}[^>]*>(.*?)<\s*\/\s*#{tag}>/m
+    #nested_regex = /<\s*#{tag}[^>]*>(.*)<\s*\/\s*#{tag}>/m
+    search_text.match(consecutive_regex)
+  end
+
+
+  def seperate_text_from_tags(node)
+    node.raw_child_tags = []
+    # while theres still tags in the nodes raw_data
+    while raw_tag = get_first_tag(node.raw_data)
+      # get the name of the search tag
+      search_tag = get_tag_name(raw_tag)
+      # find all instances of the tag, including attributes and data
+      tag = get_instance_of_tag(search_tag, node.raw_data)
+      # delete the tag from the raw_data and
+      # move it to raw_tags array for further processing
+      node.raw_child_tags << node.raw_data.slice!(tag[0])
+    end
+
+    # now all tags have been removed from raw_data
+    # leaving only non-HTML text contained in the node
+
+    # set the node's text to the remaining raw_data
+    node.text = node.raw_data.dup
+    # clean up the text, remove extra white space, new lines
+    node.text.gsub!(/\s+/, " ")
+    node.text.strip!
+
+    # clear the raw data so we know the node's ready for the next step
+    node.raw_data = nil
   end
 
 end
